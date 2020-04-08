58ffae07a731f21c5bba1327ea3f1c65204fcd1b
Patrick Lyons <X00158260@mytudublin.ie>
2020-04-08 17:49:24 +0100

    Update Person Model.

--- ContactTracingApp/ContactTracingApp/Models/Person.cs
+++ ContactTracingApp/ContactTracingApp/Models/Person.cs
@@ -2,11 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace ContactTracingApp.Models
 {
     public class Person
     {
+        [Key]
         public int id { get; set; }
+
+        [Required]
+        public string FName { get; set; }
+
+        [Required]
+        public string LName { get; set; }
+
+        [Required]
+        public int Phone { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public DateTime Dob { get; set; }
+
+
     }
 }
