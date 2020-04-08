597d2b1523577f2814b96ae45f192421fb341ae8
Keith McGloin <keithmc1810@gmail.com>
2020-04-08 17:43:52 +0100

    Contact Model Updated

--- ContactTracingApp/ContactTracingApp/Models/Contact.cs
+++ ContactTracingApp/ContactTracingApp/Models/Contact.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,16 @@ namespace ContactTracingApp.Models
 {
     public class Contact
     {
+        [Key]
+        public string ContactId { get; set; }
+        public string PersonId { get; set; }
+        public string ContactFName { get; set; }
+        public  string ContactMobile { get; set; }
+        public string ContactEmail { get; set; }
+        public string LastDateContacted { get; set; }
+        public int DistanceKept { get; set; }
+        public int TimeSpent { get; set;  }
+
     }
+
 }
