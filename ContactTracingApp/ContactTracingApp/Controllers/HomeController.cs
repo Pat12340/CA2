fcda911a6766d9f352302dd030d95b7d63f7b1a1
Keith McGloin <keithmc1810@gmail.com>
2020-04-12 19:14:46 +0100

    Updates to the homepage

--- ContactTracingApp/ContactTracingApp/Controllers/HomeController.cs
+++ ContactTracingApp/ContactTracingApp/Controllers/HomeController.cs
@@ -15,19 +15,6 @@ namespace ContactTracingApp.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
         public ActionResult ViewPeople()
         {
             ViewBag.Message = "Person List";
