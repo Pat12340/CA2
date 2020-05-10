c92bf76fc0bd29a62cbbd49dec803193ae6d9891
Patrick Lyons <X00158260@mytudublin.ie>
2020-05-10 11:46:13 +0100

    Added some unit tests to project

---
+++ ContactTracingApp/UnitTestProject/ControllerTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using ContactTracingApp.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+using ContactTracingApp;
+using ContactTracingApp.Controllers;
+
+
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class ControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            //Arrange
+            HomeController controller = new HomeController();
+
+            //Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            //Assert
+            Assert.IsNotNull(result);
+
+        }
+
+        [TestMethod]
+        public void ViewPeople()
+        {
+            //Arrange
+            HomeController controller = new HomeController();
+
+            //Act
+            ViewResult result = controller.ViewPeople() as ViewResult;
+
+            //Assert
+            Assert.AreEqual("Person List", result.ViewBag.Message);
+
+        }
+
+    }
+}
